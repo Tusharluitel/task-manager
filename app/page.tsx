5f9860008b5089a5d875a4bb57b23bc5d7d8173e
Tushar Luitel <tusharluitel88@gmail.com>
2024-03-31 07:28:33 +0545

    added sign in and sign up

--- app/page.tsx
+++
@@ -1,5 +0,0 @@
-export default function Home() {
-  return (
-    <h1 className='text-sky-500'>Hello Trello</h1>
-  )
-}
---
+++ config/site.ts
@@ -0,0 +1,4 @@
+export const siteConfig = {
+    name: "Taskify",
+    description: "Collaborate, management project and reach new productivity peaks. From high rises to home office, the way your team works is unique - accomplish it all with Taskify"
+}
